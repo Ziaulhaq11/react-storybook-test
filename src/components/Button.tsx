c0cea6ecaff2426e81d8de0a798cee469b82b8e2
Pullaray Zia <pullaray.zia@nagarro.com>
2024-11-27 21:12:52 +0530

    small changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export const Button = ({
   variant = "primary",
   size = "md",
 }: ButtonProps) => {
-  const variantStyles: Record<ButtonProps["variant"], CSSProperties> = {
+  const variantStyles: Record<ButtonProps["variant"], CSSProperties> = { //Here we're saying key type is variant type means primary or secondary. And values will be CSS properties of those keys like backgroundColor, fontSize etc.
     primary: {
       backgroundColor: "blue",
     },
@@ -39,6 +39,7 @@ export const Button = ({
         outline: "none",
         border: "none",
         cursor: "pointer",
+        color : 'white',
         borderRadius: 10,
         ...variantStyles[variant],
         ...sizeStyles[size],
